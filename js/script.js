d00dfa57ff71cd6ae4adadb11ddf52eda7ebbb1a
HemAddy <hemalatha.reachme@gmail.com>
2017-02-03 00:11:07 +0530

    made my rocket

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+var docool=function(){
+var currentclass =document.getElementById('cool').className;
+  if(currentclass == 'cool'){
+    document.getElementById('cool').className = 'cool red';
+ } else {
+    document.getElementById('cool').className ='cool'
+ }
+};
+
 var car ={
 	fly:function(){
 		alert("Helloww");
@@ -6,3 +15,4 @@ var car ={
 var whoiam=function(name){
 alert("U r"+name);
 };
+
